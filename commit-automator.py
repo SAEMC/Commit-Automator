b53f9a002961b3986d8cfefc1370884547946bd5
SAEMC <saemc27@gmail.com>
2022-10-28 00:02:56 +0900

    Create

---
+++ commit-automator.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Author: SAEMC
+# Date: 2022-10-27
+
+
+import os
+import sys
+
+# load pixel data
+
+# check pixel data today
+
+
+# check commit of remote
+
+# push commit to remote
+
+
+def main():
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
