a7d54785e0e90678245c2424c96dc7e2a25cf8ba
SAEMC <saemc27@gmail.com>
2023-07-19 02:19:20 +0900

    refactor: add line

--- commit-automator/dataloader.py
+++ commit-automator/dataloader.py
@@ -32,6 +32,7 @@ def get_github_data(*, user_name: str, access_token: str) -> dict[str, int]:
             }}
         }}
         """
+
         _response: httpx.Response = _client.post(
             url=_url, headers=_headers, json={"query": _body}
         )
